efd5e0cc11dc590e7be0b8c82c87c18aa1d793b3
parthgoyal19 <152493707+parthgoyal19@users.noreply.github.com>
2026-06-04 15:22:12 +0530

    Time: 4 ms (28.66%) | Memory: 19.3 MB (28.66%) - LeetSync

---
+++ 3611-construct-the-minimum-bitwise-array-ii/construct-the-minimum-bitwise-array-ii.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minBitwiseArray(self, nums: List[int]) -> List[int]:
+        ans = []
+        for num in nums:
+            if num == 2:
+                ans.append(-1)
+            else:
+                # Find the rightmost bit that is 1 followed by a 0 (or end of number)
+                # This corresponds to the most significant bit of the trailing continuous 1s.
+                for i in range(31):
+                    if (num >> i) & 1 == 1 and (num >> (i + 1)) & 1 == 0:
+                        # Turn off this i-th bit to get the minimized ans[i]
+                        ans.append(num ^ (1 << i))
+                        break
+        return ans
