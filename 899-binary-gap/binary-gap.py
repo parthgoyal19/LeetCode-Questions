8ca29a90f37d3c446e0f30605046b1fccbc67a5c
parthgoyal19 <152493707+parthgoyal19@users.noreply.github.com>
2026-06-05 14:46:33 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (94.55%) - LeetSync

---
+++ 899-binary-gap/binary-gap.py
@@ -0,0 +1,20 @@
+class Solution:
+    def binaryGap(self, n: int) -> int:
+        max_gap = 0
+        last_position = -1
+        current_position = 0
+        
+        while n > 0:
+            # Check if the rightmost bit is 1
+            if n & 1:
+                if last_position != -1:
+                    # Calculate the distance to the previous 1
+                    max_gap = max(max_gap, current_position - last_position)
+                # Update the last seen position of a 1 bit
+                last_position = current_position
+                
+            # Move to the next bit and increment the position counter
+            n >>= 1
+            current_position += 1
+            
+        return max_gap
