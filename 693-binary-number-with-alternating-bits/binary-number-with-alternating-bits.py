49cc5f70ec182c06d53779081ef539525f9aaa9c
parthgoyal19 <152493707+parthgoyal19@users.noreply.github.com>
2026-06-05 14:41:09 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (98.54%) - LeetSync

---
+++ 693-binary-number-with-alternating-bits/binary-number-with-alternating-bits.py
@@ -0,0 +1,7 @@
+class Solution:
+    def hasAlternatingBits(self, n: int) -> bool:
+        # If bits alternate, x will be all 1s (e.g., 101 ^ 010 = 111)
+        x = n ^ (n >> 1)
+        
+        # Check if x + 1 clears all bits when ANDed with x (e.g., 111 & 1000 == 0)
+        return (x & (x + 1)) == 0
