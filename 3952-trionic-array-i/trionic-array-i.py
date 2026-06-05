3357cf7b75b0db45b661d8e50c2e3a6677bf551a
parthgoyal19 <152493707+parthgoyal19@users.noreply.github.com>
2026-06-05 13:54:44 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (31.35%) - LeetSync

---
+++ 3952-trionic-array-i/trionic-array-i.py
@@ -0,0 +1,31 @@
+class Solution:
+    def isTrionic(self, nums: list[int]) -> bool:
+        n = len(nums)
+        if n < 3:
+            return False
+            
+        i = 0
+        
+        # 1. Walk up the first strictly increasing segment
+        while i + 1 < n and nums[i] < nums[i+1]:
+            i += 1
+            
+        # If we didn't even move, or we reached the end, it's invalid
+        if i == 0 or i == n - 1:
+            return False
+            
+        # 2. Walk down the strictly decreasing segment
+        p = i
+        while i + 1 < n and nums[i] > nums[i+1]:
+            i += 1
+            
+        # If we didn't move past p, or we reached the end, it's invalid
+        if i == p or i == n - 1:
+            return False
+            
+        # 3. Walk up the final strictly increasing segment
+        while i + 1 < n and nums[i] < nums[i+1]:
+            i += 1
+            
+        # If we successfully verified the sequence up to the last element, return True
+        return i == n - 1
