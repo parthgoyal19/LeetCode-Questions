a8afd5ac88b6e84d022e55d8a6d32f44794d8be1
parthgoyal19 <152493707+parthgoyal19@users.noreply.github.com>
2026-06-11 21:29:17 +0530

    Create 4045-LongestBalancedSubarrayI

---
+++ 4045-LongestBalancedSubarrayI/4045-LongestBalancedSubarrayI.py
@@ -0,0 +1,25 @@
+# Last updated: 11/06/2026, 21:29:15
+class Solution:
+    def longestBalanced(self, nums: list[int]) -> int:
+        n = len(nums)
+        max_len = 0
+        
+        # Check every possible starting position of the subarray
+        for i in range(n):
+            evens = set()
+            odds = set()
+            
+            # Expand the subarray to the right
+            for j in range(i, n):
+                val = nums[j]
+                
+                if val % 2 == 0:
+                    evens.add(val)
+                else:
+                    odds.add(val)
+                
+                # If the number of unique evens equals unique odds, it's balanced
+                if len(evens) == len(odds):
+                    max_len = max(max_len, j - i + 1)
+                    
+        return max_len
