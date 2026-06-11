954d2cdb2b3328c534404378ce31e24ead448235
parthgoyal19 <152493707+parthgoyal19@users.noreply.github.com>
2026-06-11 21:30:20 +0530

    Create 1910-CheckIfBinaryStringHasAtMostOneSegmentOfOnes

---
+++ 1910-CheckIfBinaryStringHasAtMostOneSegmentOfOnes/1910-CheckIfBinaryStringHasAtMostOneSegmentOfOnes.py
@@ -0,0 +1,6 @@
+# Last updated: 11/06/2026, 21:30:18
+class Solution:
+    def checkOnesSegment(self, s: str) -> bool:
+        # Since s[0] is always '1', any additional segment of '1's 
+        # would require a '0' followed by a '1' somewhere in the string.
+        return "01" not in s
