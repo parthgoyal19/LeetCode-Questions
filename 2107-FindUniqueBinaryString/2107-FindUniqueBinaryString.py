495591a90f25b495134a81e8b12a7d317de79196
parthgoyal19 <152493707+parthgoyal19@users.noreply.github.com>
2026-06-11 21:30:12 +0530

    Create 2107-FindUniqueBinaryString

---
+++ 2107-FindUniqueBinaryString/2107-FindUniqueBinaryString.py
@@ -0,0 +1,14 @@
+# Last updated: 11/06/2026, 21:30:10
+class Solution:
+    def findDifferentBinaryString(self, nums: list[str]) -> str:
+        result = []
+        
+        # Iterate through each string's corresponding diagonal element
+        for i in range(len(nums)):
+            # Invert the character at the diagonal position
+            if nums[i][i] == '0':
+                result.append('1')
+            else:
+                result.append('0')
+                
+        return "".join(result)
