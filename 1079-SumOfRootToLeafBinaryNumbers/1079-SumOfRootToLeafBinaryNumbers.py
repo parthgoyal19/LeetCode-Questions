36fb66d5188ad9886870cbaaaae25078d464268d
parthgoyal19 <152493707+parthgoyal19@users.noreply.github.com>
2026-06-11 21:31:09 +0530

    Create 1079-SumOfRootToLeafBinaryNumbers

---
+++ 1079-SumOfRootToLeafBinaryNumbers/1079-SumOfRootToLeafBinaryNumbers.py
@@ -0,0 +1,26 @@
+# Last updated: 11/06/2026, 21:31:07
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def sumRootToLeaf(self, root: Optional[TreeNode]) -> int:
+        
+        def dfs(node, current_sum):
+            if not node:
+                return 0
+            
+            # Shift left by 1 bit (multiply by 2) and add the current node's value
+            current_sum = (current_sum << 1) | node.val
+            
+            # If it's a leaf node, return the completed path value
+            if not node.left and not node.right:
+                return current_sum
+            
+            # Otherwise, sum the results from both left and right subtrees
+            return dfs(node.left, current_sum) + dfs(node.right, current_sum)
+        
+        return dfs(root, 0)
